127dc54fbc22af71e640e661f18e53d91b8b8d0e
Suchith Sridhar <suchithsridhar@gmail.com>
2023-04-12 22:41:05 -0300

    Re-formatted the test file to be a little more organized.

--- str_funcs/testing.c
+++ str_funcs/testing.c
@@ -42,70 +42,71 @@ void test_ints_equal(int out, int exp) {
 
 
 int main() {
-    bool bool_output;
-    char *input;
-    char *output;
-    char *expected;
-
     printf("\n==== Testing trim_whitespace ====\n\n");
+    {
+        char *input, *output, *expected;
 
-    input = "  lorem    ipsum  ld \n\n";
-    output = trim_whitespace(input);
-    expected = "lorem    ipsum  ld";
-    test_strings_equal(output, expected);
+        input = "  lorem    ipsum  ld \n\n";
+        output = trim_whitespace(input);
+        expected = "lorem    ipsum  ld";
+        test_strings_equal(output, expected);
 
-    input = "    hello world";
-    output = trim_whitespace(input);
-    expected = "hello world";
-    test_strings_equal(output, expected);
+        input = "    hello world";
+        output = trim_whitespace(input);
+        expected = "hello world";
+        test_strings_equal(output, expected);
 
-    input = "hello world    \n\t";
-    output = trim_whitespace(input);
-    expected = "hello world";
-    test_strings_equal(output, expected);
+        input = "hello world    \n\t";
+        output = trim_whitespace(input);
+        expected = "hello world";
+        test_strings_equal(output, expected);
 
-    input = "    \n\n \t\t   ";
-    output = trim_whitespace(input);
-    expected = "";
-    test_strings_equal(output, expected);
+        input = "    \n\n \t\t   ";
+        output = trim_whitespace(input);
+        expected = "";
+        test_strings_equal(output, expected);
 
-    input = "";
-    output = trim_whitespace(input);
-    expected = "";
-    test_strings_equal(output, expected);
+        input = "";
+        output = trim_whitespace(input);
+        expected = "";
+        test_strings_equal(output, expected);
+    }
 
     printf("\n==== Testing is_substring ====\n\n");
+    {
+        char *input;
+        bool output;
 
-    input = "hello world";
-    bool_output = is_substring(input, "llo");
-    test_true(bool_output);
+        input = "hello world";
+        output = is_substring(input, "llo");
+        test_true(output);
 
-    input = "hello world";
-    bool_output = is_substring(input, "lol");
-    test_true(!bool_output);
+        input = "hello world";
+        output = is_substring(input, "lol");
+        test_true(!output);
 
-    input = "abc";
-    bool_output = is_substring(input, "abc");
-    test_true(bool_output);
+        input = "abc";
+        output = is_substring(input, "abc");
+        test_true(output);
 
-    input = "";
-    bool_output = is_substring(input, "abc");
-    test_true(!bool_output);
+        input = "";
+        output = is_substring(input, "abc");
+        test_true(!output);
 
-    input = "";
-    bool_output = is_substring(input, "");
-    test_true(bool_output);
+        input = "";
+        output = is_substring(input, "");
+        test_true(output);
 
-    input = "adsf";
-    bool_output = is_substring(input, "");
-    test_true(bool_output);
+        input = "adsf";
+        output = is_substring(input, "");
+        test_true(output);
 
-    input = "oodoodooo";
-    bool_output = is_substring(input, "dooo");
-    test_true(bool_output);
+        input = "oodoodooo";
+        output = is_substring(input, "dooo");
+        test_true(output);
+    }
 
     printf("\n==== Testing index_of ====\n\n");
-
     {
         char *input, *substring;
         int start, output, expected;
@@ -169,7 +170,6 @@ int main() {
 
 
     printf("\n==== Testing index_of with varying start indices ====\n\n");
-
     {
         int start, output, expected;
         char *input, *substring;
@@ -240,88 +240,97 @@ int main() {
     }
 
     printf("\n==== Testing get_substring ====\n\n");
+    {
+        char *input, *output, *expected;
 
-    // Reverse a string
-    input = "hello world";
-    output = get_substring(input, -1, 0, -1);
-    expected = "dlrow olleh";
-    test_strings_equal(output, expected);
+        // Reverse a string
+        input = "hello world";
+        output = get_substring(input, -1, 0, -1);
+        expected = "dlrow olleh";
+        test_strings_equal(output, expected);
 
-    // Forward direction substring
-    input = "helloworld";
-    output = get_substring(input, 2, 7, 1);
-    expected = "llowor";
-    test_strings_equal(output, expected);
+        // Forward direction substring
+        input = "helloworld";
+        output = get_substring(input, 2, 7, 1);
+        expected = "llowor";
+        test_strings_equal(output, expected);
 
-    // Backward direction substring
-    input = "helloworld";
-    output = get_substring(input, 7, 2, -1);
-    expected = "rowoll";
-    test_strings_equal(output, expected);
+        // Backward direction substring
+        input = "helloworld";
+        output = get_substring(input, 7, 2, -1);
+        expected = "rowoll";
+        test_strings_equal(output, expected);
 
-    // Slice with negative indices
-    input = "helloworld";
-    output = get_substring(input, -6, -3, 1);
-    expected = "owor";
-    test_strings_equal(output, expected);
+        // Slice with negative indices
+        input = "helloworld";
+        output = get_substring(input, -6, -3, 1);
+        expected = "owor";
+        test_strings_equal(output, expected);
 
-    // Slice with negative indices reverse step
-    input = "helloworld";
-    output = get_substring(input, -3, -6, -1);
-    expected = "rowo";
-    test_strings_equal(output, expected);
+        // Slice with negative indices reverse step
+        input = "helloworld";
+        output = get_substring(input, -3, -6, -1);
+        expected = "rowo";
+        test_strings_equal(output, expected);
 
-    // Slice with higher step multiple : 3
-    input = "helloworld";
-    output = get_substring(input, 0, -1, 3);
-    expected = "hlod";
-    test_strings_equal(output, expected);
+        // Slice with higher step multiple : 3
+        input = "helloworld";
+        output = get_substring(input, 0, -1, 3);
+        expected = "hlod";
+        test_strings_equal(output, expected);
 
-    // Slice with higher step multiple : 2
-    input = "helloworld";
-    output = get_substring(input, 0, -1, 2);
-    expected = "hlool";
-    test_strings_equal(output, expected);
+        // Slice with higher step multiple : 2
+        input = "helloworld";
+        output = get_substring(input, 0, -1, 2);
+        expected = "hlool";
+        test_strings_equal(output, expected);
 
-    // Slice with higher step multiple : 3
-    input = "helloworld";
-    output = get_substring(input, -1, 0, -3);
-    expected = "dolh";
-    test_strings_equal(output, expected);
+        // Slice with higher step multiple : 3
+        input = "helloworld";
+        output = get_substring(input, -1, 0, -3);
+        expected = "dolh";
+        test_strings_equal(output, expected);
 
-    // Slice with higher step multiple : 2
-    input = "helloworld";
-    output = get_substring(input, -1, 0, -2);
-    expected = "drwle";
-    test_strings_equal(output, expected);
+        // Slice with higher step multiple : 2
+        input = "helloworld";
+        output = get_substring(input, -1, 0, -2);
+        expected = "drwle";
+        test_strings_equal(output, expected);
+    }
 
     printf("\n==== Testing to_uppercase ====\n\n");
+    {
+        char *input, *output, *expected;
 
-    // Convert all characters to uppercase
-    input = "helloworld";
-    output = to_uppercase(input);
-    expected = "HELLOWORLD";
-    test_strings_equal(output, expected);
+        // Convert all characters to uppercase
+        input = "helloworld";
+        output = to_uppercase(input);
+        expected = "HELLOWORLD";
+        test_strings_equal(output, expected);
 
-    // Convert only lowercase characters to uppercase
-    input = " h *8e  ]13057o W '/.;' OrLd";
-    output = to_uppercase(input);
-    expected = " H *8E  ]13057O W '/.;' ORLD";
-    test_strings_equal(output, expected);
+        // Convert only lowercase characters to uppercase
+        input = " h *8e  ]13057o W '/.;' OrLd";
+        output = to_uppercase(input);
+        expected = " H *8E  ]13057O W '/.;' ORLD";
+        test_strings_equal(output, expected);
+    }
 
     printf("\n==== Testing to_lowercase ====\n\n");
+    {
+        char *input, *output, *expected;
 
-    // Convert all characters to lowercase
-    input = "HELLOWORLD";
-    output = to_lowercase(input);
-    expected = "helloworld";
-    test_strings_equal(output, expected);
+        // Convert all characters to lowercase
+        input = "HELLOWORLD";
+        output = to_lowercase(input);
+        expected = "helloworld";
+        test_strings_equal(output, expected);
 
-    // Convert only uppercase characters to lowercase
-    input = " h *8e  ]13057o W '/.;' OrLd";
-    output = to_lowercase(input);
-    expected = " h *8e  ]13057o w '/.;' orld";
-    test_strings_equal(output, expected);
+        // Convert only uppercase characters to lowercase
+        input = " h *8e  ]13057o W '/.;' OrLd";
+        output = to_lowercase(input);
+        expected = " h *8e  ]13057o w '/.;' orld";
+        test_strings_equal(output, expected);
+    }
 
     return 0;
 }
