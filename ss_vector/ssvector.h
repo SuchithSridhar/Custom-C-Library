96e54e60f69a957107eeb0feb3c6d1e2a0692242
Suchith Sridhar <suchithsridhar@gmail.com>
2023-12-21 20:10:46 -0400

    Forgot to update the struct name in the comments!

--- ss_vector/ssvector.h
+++ ss_vector/ssvector.h
@@ -25,14 +25,14 @@ ssv_vector_t* ssv_init(size_t memb_size, size_t init_size);
 
 /**
  * Destroy the vector and free its memory.
- * @param vec the ss_Vector to destroy.
+ * @param vec the ssv_vector_t to destroy.
  * @return true if the operation is successful, false otherwise.
  */
 bool ssv_destroy(ssv_vector_t *vec);
 
 /**
  * Get an element from the vector at a specified index.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param index the index of the element to be retrieved.
  * @return a pointer within the vector to the element if found, NULL otherwise.
  */
@@ -40,7 +40,7 @@ void* ssv_get(ssv_vector_t *vec, size_t index);
 
 /**
  * Push an element to the end of the vector.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param item the element to be added.
  * @return true if the operation is successful, false otherwise.
  */
@@ -48,7 +48,7 @@ bool ssv_push(ssv_vector_t *vec, void *item);
 
 /**
  * Get an element from the end of the vector.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @return a pointer within the vector to the element if exists, else NULL.
  */
 void* ssv_peek(ssv_vector_t *vec);
@@ -56,7 +56,7 @@ void* ssv_peek(ssv_vector_t *vec);
 /**
  * Pop an element from the end of the vector.
  * The returned element needs to be freed.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @return a pointer to the popped element (on heap), NULL if fails.
  */
 void* ssv_pop(ssv_vector_t *vec);
@@ -65,14 +65,14 @@ void* ssv_pop(ssv_vector_t *vec);
  * Clear all the elements of the array.
  * Note that this only updated the size
  * and does not actually clear the array.
- * @param vec the ss_Vector to clear.
+ * @param vec the ssv_vector_t to clear.
  * @return true if operation is successful, false otherwise.
  */
 bool ssv_clear(ssv_vector_t *vec);
 
 /**
  * Delete an element from the end of the vector.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @return true if operation is successful, false otherwise.
  */
 bool ssv_delete(ssv_vector_t *vec);
@@ -80,14 +80,14 @@ bool ssv_delete(ssv_vector_t *vec);
 /**
  * Delete an element at the specified index.
  * @param index the index at which the element is to be deleted.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @return true if operation is successful, false otherwise.
  */
 bool ssv_delete_at(ssv_vector_t *vec, size_t index);
 
 /**
  * Pop an element from the vector at a specified index.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param index the index at which the element is to be popped.
  * @return a pointer to the popped element, NULL otherwise.
  */
@@ -95,7 +95,7 @@ void* ssv_pop_at(ssv_vector_t *vec, size_t index);
 
 /**
  * Push an element into the vector at a specified index.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param item the element to be inserted.
  * @param index the index at which the element is to be inserted.
  * @return true if the operation is successful, false otherwise.
@@ -104,7 +104,7 @@ bool ssv_push_at(ssv_vector_t *vec, void *item, size_t index);
 
 /**
  * Replace an element in the vector at a specified index with another element.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param item the new element to insert.
  * @param index the index of the element to be replaced.
  * @return true if the operation is successful, false otherwise.
@@ -113,7 +113,7 @@ bool ssv_replace_at(ssv_vector_t *vec, void *item, size_t index);
 
 /**
  * Find the index of an element in the vector.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param item the element to search for.
  * @return the index of the element if found, SIZE_MAX otherwise.
  */
@@ -121,14 +121,14 @@ size_t ssv_index_of(ssv_vector_t *vec, void *item);
 
 /**
  * Print the elements inside the vector.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param print_elem a function pointer to print an element.
  */
 void ssv_print(ssv_vector_t *vec, void (*print_elem) (void*));
 
 /**
  * Sort the vector based on the provided compare function.
- * @param vec the ss_Vector to operate on.
+ * @param vec the ssv_vector_t to operate on.
  * @param compare a function point to a function to compare two elements.
  */
 void ssv_sort(ssv_vector_t *vec, int (*compare)(const void*, const void*));
